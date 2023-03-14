123afab705f1a88175fcba551274c2b52932821e
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-03-14 11:48:09 -0400

    add support code for webview

--- lesson06_lab/lib/src/screens/web_screen.dart
+++ lesson06_lab/lib/src/screens/web_screen.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:lesson06_lab/src/widgets/widgets.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class WebScreen extends StatelessWidget {
+class WebScreen extends StatefulWidget {
   String url;
   WebScreen({Key? key, required this.url}) : super(key: key);
 
   @override
+  _WebScreenState createState() => _WebScreenState();
+}
+
+class _WebScreenState extends State<WebScreen> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onProgress: (int progress) {
+          print("WebView is loading (progress : $progress%)");
+        },
+        onNavigationRequest: (NavigationRequest request) {
+          print("Opening ${request.url}");
+          return NavigationDecision.navigate;
+        },
+      ))
+      ..loadRequest(Uri.parse(widget.url));
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(context),
-      body: webViewBase(url),
+      appBar: AppBar(
+          backgroundColor: Colors.deepOrangeAccent,
+          title: Text('Website: ${widget.url.split('//')[1]}')),
+      body: WebViewWidget(
+        controller: controller,
+      ),
     );
   }
 }
