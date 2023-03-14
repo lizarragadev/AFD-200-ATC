42ac5d9e12a3cf8d87049bf29e0bd72a766378c5
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-03-13 22:11:40 -0400

    add base code sample drawers

--- lesson07_lab/lib/src/screens/drawers_screen.dart
+++ lesson07_lab/lib/src/screens/drawers_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class DrawersScreen extends StatelessWidget {
+class DrawersScreen extends StatefulWidget {
   const DrawersScreen({Key? key}) : super(key: key);
 
   @override
+  _DrawersScreenState createState() => _DrawersScreenState();
+}
+
+class _DrawersScreenState extends State<DrawersScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+      drawer: Drawer(
+        child: Column(children: [
+          Text("asdadasdas"),
+          SizedBox(height: 10),
+          Icon(Icons.star),
+          Text("asdadasdas"),
+          SizedBox(height: 10),
+          Icon(Icons.star),
+        ]),
+      ),
+    );
   }
 }
