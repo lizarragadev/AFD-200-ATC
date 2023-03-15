8dbd311ad44a200ca36f17f24faccb8644e7794e
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-03-14 22:28:55 -0400

    add provider classs

---
+++ lesson07_lab/lib/src/providers/people_provider.dart
@@ -0,0 +1,16 @@
+// Main Thread  o  UI Thread
+// Thread, Async, AsyncTask, Coroutines,  => Future <=
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class PeopleProvider {
+  List<dynamic> lista = [];
+
+  Future<List<dynamic>> cargarDatos() async {
+    final response = await rootBundle.loadString("assets/data/people.json");
+    Map dataMap = json.decode(response);
+    lista.addAll(dataMap['people']);
+    return lista;
+  }
+}
